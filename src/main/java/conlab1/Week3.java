7444b24716187d747e4fb9c4203120679e9f5dee
rory619 <G00422319@atu.ie>
2024-10-01 10:53:51 +0100

    adding new changes

---
+++ src/main/java/conlab1/Week3.java
@@ -0,0 +1,20 @@
+package ie.atu.conlab1;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class Week3 {
+
+    @GetMapping("/message")
+    public String getMessage()
+    {
+        return "Some message";
+    }
+
+    @GetMapping("/details")
+    public String getDetails()
+    {
+        return "Some other message";
+    }
+}
---
+++ src/test/java/conlab1/Conlab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package conlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Conlab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
